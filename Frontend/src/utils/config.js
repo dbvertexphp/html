4ab7b0946d9221d26fd26edcf922979637bc647c
dbvertexphp <128798599+dbvertexphp@users.noreply.github.com>
2023-12-01 19:04:59 +0530

    changes live

--- Frontend/src/utils/config.js
+++ Frontend/src/utils/config.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = "https://easygocarz.com";
-export const BASE_URL = "http://65.1.64.251";
+//export const BASE_URL = "http://65.1.64.251";
+export const BASE_URL = "http://localhost:3000";
