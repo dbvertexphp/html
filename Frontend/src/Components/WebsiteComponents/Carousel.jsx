6ac6186f208abb67f615e6d7f14c8a47b0eabb24
dbvertexphp <128798599+dbvertexphp@users.noreply.github.com>
2023-12-05 10:50:23 +0530

    changes live

--- Frontend/src/Components/WebsiteComponents/Carousel.jsx
+++ Frontend/src/Components/WebsiteComponents/Carousel.jsx
@@ -1,18 +1,11 @@
-import { useEffect, useState } from "react";
-import {
-  Box,
-  Heading,
-  Image,
-  InputGroup,
-  Skeleton,
-  Text,
-} from "@chakra-ui/react";
-import Slider from "react-slick";
-import { useDispatch } from "react-redux";
-import { getallBanners } from "../../Redux/App/Actions/Admin/Website/Website.action";
-import AsyncSelector from "../Extra/AsyncSelect";
-import { getAllCarNamess } from "../../Redux/App/Actions/Admin/CarComponents/CarName.action";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { Box, Heading, Image, InputGroup, Skeleton, Text } from '@chakra-ui/react';
+import Slider from 'react-slick';
+import { useDispatch } from 'react-redux';
+import { getallBanners } from '../../Redux/App/Actions/Admin/Website/Website.action';
+import AsyncSelector from '../Extra/AsyncSelect';
+import { getAllCarNamess } from '../../Redux/App/Actions/Admin/CarComponents/CarName.action';
+import { useNavigate } from 'react-router-dom';
 
 const settings = {
   dots: true,
@@ -23,7 +16,7 @@ const settings = {
   speed: 500,
   autoplaySpeed: 5000,
   slidesToShow: 1,
-  slidesToScroll: 1,
+  slidesToScroll: 1
 };
 
 export default function Carousel() {
@@ -31,97 +24,60 @@ export default function Carousel() {
   const [cards, setcards] = useState([]);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const getData = (e) => {
+  const getData = e => {
     dispatch(getallBanners(null, setcards));
   };
-  const handleSearchChange = (val) => {
+  const handleSearchChange = val => {
     let data = { search: { name: val._id } };
 
-    navigate("/collection");
+    navigate('/collection');
   };
   useEffect(() => {
     getData();
   }, []);
   return (
-    <Box
-      position={"relative"}
-      width={"100%"}
-      maxHeight={{ base: "850px", md: "550px" }}
-      overflow={"hidden"}
-      margin="auto"
-    >
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-      />
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-      />
+    <>
+      <Box position={'relative'} width={'100%'} maxHeight={{ base: '850px', md: '850px' }} overflow={'hidden'} margin="auto">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
 
-      <Box
-        zIndex={1}
-        left={{ base: "10px", md: "150px" }}
-        bottom={{ base: "40px", md: "120px" }}
-        position={"absolute"}
-      >
-        <Heading
-          fontWeight={"bold"}
-          fontSize={"34"}
-          color="White"
-          whiteSpace="nowrap"
-          my="5"
-        >
-          EasyGoCarz
-        </Heading>
-        <Text
-          fontWeight={{ md: "bold" }}
-          fontSize={{ base: "11", md: "20" }}
-          color="White"
-          whiteSpace="nowrap"
-          mb="10"
-          backdropFilter="blur(1px)"
-        >
-          Unlock Your Next Journey with Our Trusted Pre-Owned Cars!
-        </Text>
+        <Box zIndex={1} left={{ base: '10px', md: '150px' }} bottom={{ base: '40px', md: '120px' }} position={'absolute'}>
+          <Heading fontWeight={'bold'} fontSize={'34'} color="White" whiteSpace="nowrap" my="5">
+            find your dreams car within a minute
+          </Heading>
+          <Text fontWeight={{ md: 'bold' }} fontSize={{ base: '11', md: '20' }} color="White" whiteSpace="nowrap" mb="10" backdropFilter="blur(1px)">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+          </Text>
+        </Box>
 
-        <InputGroup w={{ base: "full", md: "650px" }} size="lg">
-          <AsyncSelector
-            w="100%"
-            handleChangeFn={handleSearchChange}
-            getItems={getAllCarNamess}
-            placeholder={"Search Car"}
-            bg="gray.200"
-          />
-        </InputGroup>
+        <Slider {...settings} ref={slider => setSlider(slider)}>
+          {cards.length == 0 ? (
+            <Skeleton height="500px"></Skeleton>
+          ) : (
+            cards.map((image, index) => (
+              <Box key={index} display="flex" justifyContent="center" alignItems="center" height="100%">
+                <Image
+                  width="100%"
+                  h={{ base: '250px', md: '850px' }}
+                  objectFit="cover"
+                  objectPosition="center"
+                  src={image}
+                  // filter={"blur(1px)"}
+                />
+              </Box>
+            ))
+          )}
+        </Slider>
       </Box>
 
-      <Slider {...settings} ref={(slider) => setSlider(slider)}>
-        {cards.length == 0 ? (
-          <Skeleton height="500px"></Skeleton>
-        ) : (
-          cards.map((image, index) => (
-            <Box
-              key={index}
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              height="100%"
-            >
-              <Image
-                width="100%"
-                h={{ base: "250px", md: "550px" }}
-                objectFit="cover"
-                objectPosition="center"
-                src={image}
-                // filter={"blur(1px)"}
-              />
-            </Box>
-          ))
-        )}
-      </Slider>
-    </Box>
+      <Box zIndex={1} left={{ base: '10px', md: '150px' }} bottom={{ base: '40px', md: '120px' }}>
+        <Heading className="serach_heading" my="5">
+          Select your car brand to get started
+        </Heading>
+        <InputGroup w={{ base: 'full', md: '650px' }} size="lg">
+          <AsyncSelector w="100%" handleChangeFn={handleSearchChange} getItems={getAllCarNamess} placeholder={'Search Car'} bg="gray.200" />
+        </InputGroup>
+      </Box>
+    </>
   );
 }
